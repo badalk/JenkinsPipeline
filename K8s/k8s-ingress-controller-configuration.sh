9dc9d5f8d2b2f0205c2e714082a290e05e7b147a
Badal Kotecha <badal.kotecha@mindtree.com>
2018-10-19 14:54:37 -0400

    removed shell script and changed path in jenkins

--- K8s/k8s-ingress-controller-configuration.sh
+++
@@ -1,8 +0,0 @@
-aksResourceGroup=$1
-aksName=$2
-
-nodeResourceGroup="$(az aks show --resource-group ${aksResourceGroup} --name ${aksName} --query nodeResourceGroup -o tsv)"
-
-echo "${nodeResourceGroup}"
-
-az network public-ip create --resource-group "${nodeResourceGroup}" --name aksIngressPublicIP --allocation-method static
